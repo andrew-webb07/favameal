42a7623984145dad5a91d038753891e90b44d9ca
Andrew Webb <webba@ihopu.org>
2021-12-22 11:26:23 -0600

    added ability for user to rate meals

--- favamealapi/models/mealrating.py
+++ favamealapi/models/mealrating.py
@@ -1,10 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class MealRating(models.Model):
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name="mealrating")
-    meal = models.ForeignKey(
-        "Meal", on_delete=models.CASCADE, related_name="mealrating")
-    rating = models.IntegerField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="mealrating")
+    meal = models.ForeignKey("Meal", on_delete=models.CASCADE, related_name="mealrating")
+    rating = models.IntegerField(default=1, validators=[MaxValueValidator(10), MinValueValidator(1)])
